e090ae1da5841604ffef252c1375b48f0304bb3e
Mindaugas Jačionis <mindaugasjacionis@Mindaugass-MacBook-Pro.local>
2017-02-16 17:44:00 +0200

    Fully renderable calendar

--- components/Calendar.js
+++ components/Calendar.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import { View, ScrollView, Text, TouchableOpacity StyleSheet, PixelRatio } from 'react-native';
+import { View, ScrollView, Text, TouchableOpacity, StyleSheet, PixelRatio } from 'react-native';
 import moment from 'moment';
 import _ from 'underscore';
 
@@ -18,7 +18,7 @@ class Calendar extends Component {
     startDate: moment(),
     unavailable: [],
     selected: moment(),
-    headerStyle {},
+    headerStyle: {},
     monthsCount: 3,
     weekHeadings: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
     monthNames: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
@@ -32,14 +32,14 @@ class Calendar extends Component {
     const { weekHeadings, headerStyle } = this.props;
     const headings = _.map(weekHeadings , (heading) => {
       return (
-        <View style={styles.row}>
+        <View style={styles.default}>
           <Text style={headerStyle}>{heading}</Text>
         </View>
       );
     });
 
     return (
-      <View style={[styles.header, headerStyle]}>
+      <View style={[styles.row, headerStyle]}>
         {headings}
       </View>
     );
@@ -71,7 +71,7 @@ class Calendar extends Component {
 
     return (
       <View>
-        <Text>{`${currentMonth} ${year}`}</Text>
+        <Text style={styles.default}>{`${currentMonth} ${year}`}</Text>
         <View>
           {month}
         </View>
@@ -84,18 +84,26 @@ class Calendar extends Component {
     let weeks = [];
     let days = [];
 
+    console.log('Maybe it is const', monthDaysCount);
     for (let i = 1; i <= monthDaysCount; i++) {
-      if (Number(date.date(i).day()) === 1) {
-        const method = i < 7 ? 'unshift' : 'concat';
-        const day = this.renderDay(i);
-        const daysOfWeek = days.length !== 7 ? this.fillInWeek(days) : days;
+      console.log('loop', i, monthDaysCount);
+      if (Number(date.date(i).day()) === 1 && i < monthDaysCount) {
+        const day = this.renderDay(i, date);
+        const daysOfWeek = days.length !== 7 ? this.fillInWeek(days, 'unshift') : days;
         const weekRow = <View style={styles.row}>{daysOfWeek}</View>;
         weeks = _.union(weeks, [weekRow]);
         days = _.union([], [day]);
+      } else if (i === monthDaysCount) {
+        const day = this.renderDay(i, date);
+        days = _.union(days, [day]);
+        const daysOfWeek = days.length !== 7 ? this.fillInWeek(days, 'push') : days;
+        const weekRow = <View style={styles.row}>{daysOfWeek}</View>;
+        weeks = _.union(weeks, [weekRow]);
       } else {
         let day = this.renderDay(i, date);
         days = _.union(days, [day]);
       }
+      console.log('end of cicle', weeks, days);
     }
 
     return weeks;
@@ -103,16 +111,19 @@ class Calendar extends Component {
 
   renderDay(dayNumber, date) {
     return (
-      <TouchableOpacity onPress={() => this.props.onSelect(date.date(i).toDate())} style={styles.day}>
-        <Text>{dayNumber}</Text>
+      <TouchableOpacity style={styles.default} onPress={() => this.props.onSelect(date.date(dayNumber).toDate())}>
+        <Text>
+          {dayNumber}
+        </Text>
       </TouchableOpacity>
     );
   }
 
   fillInWeek(days, method) {
+    console.log('method', method, days);
     let week = days;
     while (week.length !== 7) {
-      week = week[method](<View style={styles.filler} />);
+      week[method](<View style={styles.default} />);
     }
 
     return week;
@@ -122,7 +133,7 @@ class Calendar extends Component {
     const { startDate, unavailable, selected, headerStyle, monthsCount } = this.props;
 
     return (
-      <View>
+      <View style={styles.container}>
         {this.renderHeader()}
         <ScrollView style={styles.container}>
           {this.renderCalendars()}
@@ -138,14 +149,17 @@ var styles = StyleSheet.create({
   },
 
   default:{
-    flex:1,
+    flex: 1,
+    backgroundColor: 'transparent',
     alignItems:'center',
     justifyContent:'center',
+    textAlign: 'center',
+    borderColor: 'green',
+    borderWidth: 1
   },
 
   row: {
     flexDirection: 'row',
-    flex: 1
   }
 });
 
